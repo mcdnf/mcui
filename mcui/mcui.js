a49ea8f543650293aedf45d9f0f02cba6184808b
mcdnf <190764017@qq.com>
2015-10-21 23:22:29 +0800

    微调

--- mcui/mcui.js
+++ mcui/mcui.js
@@ -1,7 +1,4 @@
 (function() {
-	if(isMobile()) {
-		document.getElementsByTagName('body')[0].className = 'app';
-	}
 	function isMobile() {
 		return navigator.userAgent.match(/Mobile|iPhone|iPad|Android/i) || Math.min(screen.height,screen.width) <= 480;
 	}
